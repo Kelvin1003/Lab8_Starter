11a551970d96f76beb77d9e0a4850e3c03a363cd
Kelvin1003 <kay003@ucsd.edu>
2021-11-14 03:10:59 -0800

    Finish

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Lab 8 - Starter
+
+## Name: Kangming Yu
+## PID: A16528320
+
+<br />
+<hr />
+<br />
+
+### Check Your Understanding
+1. I will fit the automated tests within a Github action that runs whenever code is pushed 
+    - Because it fits the definition of automation and piping. When we complete part of the task and push the code to Github, the test can be triggered automatically.
+
+<br />
+
+2. No
+
+ <br />
+
+3. No. 
+    - I think it takes more than one unit to implement "message" feature. In other words, there will be interactions between multiple units of "message" feature. Therefore, unit test is not suitable for testing the entire "message" feature.
+
+<br />
+
+4. Yes.
+    - The maximum message length feature should be implemented within a unit. Such feature does not require the interaction of multiple units. We can use unit test to simply test the unit to implement the maximum message length feature. It's also very convenient and execute quickly.
+
+<br />
+<hr />
+<br />
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,111 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// Test: isPhoneNumber(phoneNumber)
+
+// 2 tests that should be true
+test('900-300-7000 is a valid phone number.', () => {
+    expect(functions.isPhoneNumber('900-300-7000')).toBe(true);
+});
+
+test('(900) 300-7000 is a valid phone number.', () => {
+    expect(functions.isPhoneNumber('(900) 300-7000')).toBe(true);
+});
+
+// 2 tests that should be false
+test('000 000 00000000 is not a valid phone number.', () => {
+    expect(functions.isPhoneNumber('000 000 00000000')).toBe(false);
+});
+
+test('1111 is not a valid phone number.', () => {
+    expect(functions.isPhoneNumber('1111')).toBe(false);
+});
+
+
+
+// Test: isEmail(email)
+
+// 2 tests that should be true
+test('yyy@gmail.com is a valid email.', () => {
+    expect(functions.isEmail('yyy@gmail.com')).toBe(true);
+});
+
+test('yy111_y@qq.com is a valid email.', () => {
+    expect(functions.isEmail('yy111_y@qq.com')).toBe(true);
+});
+
+// 2 tests that should be false
+test('1111qq.com is not a valid email.', () => {
+    expect(functions.isEmail('1111qq.com')).toBe(false);
+});
+
+test('1111qq is not a valid email.', () => {
+    expect(functions.isEmail('1111qq')).toBe(false);
+});
+
+
+
+// Test: isStrongPassword(password)
+
+// 2 tests that should be true
+test('yyyy121 is a strong password.', () => {
+    expect(functions.isStrongPassword('yyyy121')).toBe(true);
+});
+
+test('yyyy121_222 is a strong password.', () => {
+    expect(functions.isStrongPassword('yyyy121_222')).toBe(true);
+});
+
+// 2 tests that should be false
+test('0000000 is not a strong password.', () => {
+    expect(functions.isStrongPassword('0000000')).toBe(false);
+});
+
+test('a is not a strong password.', () => {
+    expect(functions.isStrongPassword('a')).toBe(false);
+});
+
+
+
+// Test: isDate(date)
+
+// 2 tests that should be true
+test('01/1/1999 is a date.', () => {
+    expect(functions.isDate('01/1/1999')).toBe(true);
+});
+
+test('12/12/1999 is a date.', () => {
+    expect(functions.isDate('12/12/1999')).toBe(true);
+});
+
+// 2 tests that should be false
+test('01/1eee/1999 is not a date.', () => {
+    expect(functions.isDate('01/1eee/1999')).toBe(false);
+});
+
+test('01/11212/1999 is a date.', () => {
+    expect(functions.isDate('01/11212/1999')).toBe(false);
+});
+
+
+
+// Test: isHexColor(color)
+
+// 2 tests that should be true
+test('#000000 is a hex color.', () => {
+    expect(functions.isHexColor('#000000')).toBe(true);
+});
+
+test('#aabbcc is a hex color.', () => {
+    expect(functions.isHexColor('#aabbcc')).toBe(true);
+});
+
+// 2 tests that should be false
+test('#------ is not a hex color.', () => {
+    expect(functions.isHexColor('#------')).toBe(false);
+});
+
+test('#qqxxxxxx is not a hex color.', () => {
+    expect(functions.isHexColor('#qqxxxxxx')).toBe(false);
+});
